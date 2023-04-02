be32afa20cf4b84de5ef94f57d26e711fbe7b841
walig-here <daw.walig@gmail.com>
2023-04-02 17:14:35 +0200

    Implementacja kanału transmisyjnego
    Zaprogramowanie działania bazowaego kanału transmisyjnego.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import numpy
+
+import arq.Channel as ch
+
+
 # Entry point narzędzia symulacyjnego
 def main():
-    print("Hello world!")
+    channel = ch.Channel(0.1)
+    segment = numpy.array([1, 0, 1])
+    channel.send_segment(segment)
+    segment = channel.receive_segment()
+    print(segment)
     
 
 if __name__ == "__main__":
