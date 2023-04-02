bbac0738745132478f2a34fe13fd7e8020fde3fe
walig-here <daw.walig@gmail.com>
2023-04-02 17:26:03 +0200

    Przywrócenie starego maina

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import numpy
-
-import arq.Channel as ch
-
-
 # Entry point narzędzia symulacyjnego
 def main():
-    channel = ch.Channel(0.1)
-    segment = numpy.array([1, 0, 1])
-    channel.send_segment(segment)
-    segment = channel.receive_segment()
-    print(segment)
-    
+    print("Hello world!")
+
 
 if __name__ == "__main__":
     main()
