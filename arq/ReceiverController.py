e5ba6292aa4ee86e1da5b3a74258a998fbef0a7a
MaciuSkovsky23 <107055716+MaciuSkovsky23@users.noreply.github.com>
2023-04-04 21:14:53 +0200

    Update ReceiverController.py

--- arq/ReceiverController.py
+++ arq/ReceiverController.py
@@ -6,8 +6,16 @@
 
 import numpy
 
+from arq import Receiver
+
 
 class ReceiverController:
+
+    def __init__(self, receiver: Receiver):
+        self.receiver = receiver
+        self.messages_queue = []
+        self.responses_queue = []
+
     # -----------------------------------------------------------------------
     # Wprowadza wiadomość bitową do kontrolera.
     #
@@ -18,7 +26,7 @@ class ReceiverController:
     # Nic.
     # -----------------------------------------------------------------------
     def push_message(self, message: numpy.array):
-        return
+        self.messages_queue.append(message)
 
     # -----------------------------------------------------------------------
     # Wyciąga z kontrolera odpowiedź, która ma zostać przesłana kanałem
@@ -29,7 +37,9 @@ class ReceiverController:
     # False, gdy jest to żądanie retransmisji.
     # -----------------------------------------------------------------------
     def pop_response(self) -> bool:
-        return True
+        if not self.responses_queue:
+            return None
+        return self.responses_queue.pop(0)
 
     # -----------------------------------------------------------------------
     # Wyciąga wiadomość bitową z kontrolera.
@@ -38,5 +48,49 @@ class ReceiverController:
     # Wiadomość bitową z pamięci kontrolera.
     # -----------------------------------------------------------------------
     def pop_message(self) -> numpy.array:
-        return numpy.array(0)
+        if not self.messages_queue:
+            return None
+        return self.messages_queue.pop(0)
 
+    # -----------------------------------------------------------------------
+    # Przetwarza otrzymane wiadomości, sprawdza błędy i generuje odpowiedzi.
+    #
+    # Zwraca:
+    # Nic.
+    # -----------------------------------------------------------------------
+
+    def process_messages(self):
+        while self.messages_queue:
+            message = self.messages_queue.pop(0)
+            original_message = self.decode_message(message)
+            self.receiver.receive_message(message, original_message)
+            self.responses_queue.append(self.check_errors(message, original_message))
+
+    # -----------------------------------------------------------------------
+    # Sprawdza, czy wystąpiły błędy w otrzymanej wiadomości, porównując ją
+    # z oryginalną wiadomością.
+    #
+    # Parametry:
+    # received_message - wiadomość bitowa otrzymana przez odbiorcę
+    # original_message - oryginalna wiadomość bitowa wysłana przez nadawcę
+    #
+    # Zwraca:
+    # True, gdy otrzymana wiadomość jest równa oryginalnej wiadomości.
+    # False, gdy wystąpiły błędy.
+    # ----------------------------------------------------------------------
+    def check_errors(self, received_message: numpy.array, original_message: numpy.array) -> bool:
+        return numpy.array_equal(received_message, original_message)
+
+    # -----------------------------------------------------------------------
+    # Dekoduje wiadomość przy użycciu kodów BCH.
+    # #
+    # # Parametry:
+    # # message - wiadomość bitowa do zdekodowania
+    # #
+    # # Zwraca:
+    # # Oryginalną wiadomość bitową po dekodowaniu.
+    # # -----------------------------------------------------------------------
+
+    def decode_message(self, message:numpy.array) -> numpy.array:
+        #implementacja dekodowania wiadomości przy użyciu kodów BCH
+        return numpy.array(0)
