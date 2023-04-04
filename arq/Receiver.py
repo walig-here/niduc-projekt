1478f439ea0ceeb87b77a8f22ea27c6cf25620ed
MaciuSkovsky23 <107055716+MaciuSkovsky23@users.noreply.github.com>
2023-04-04 21:12:16 +0200

    Update Receiver.py

--- arq/Receiver.py
+++ arq/Receiver.py
@@ -3,11 +3,16 @@
 # - Agreguje statystyki na temat przeprowadzonej transmisji
 # - Zapisuje statystyki do pliku *.csv
 
+import csv
 
 import numpy
 
 
 class Receiver:
+
+    def __init__(self):
+        self.error_count = 0
+        self.received_messages = []
     # -----------------------------------------------------------------------
     # Odbiera wiadomość z kontrolera transmisji oraz oryginalną wiadomość
     # od nadawcy.
@@ -20,7 +25,9 @@ class Receiver:
     # Nic.
     # -----------------------------------------------------------------------
     def receive_message(self, received_message: numpy.array, original_message: numpy.array):
-        return
+        self.received_messages.append((received_message, original_message))
+        errors = numpy.sum(received_message != original_message)
+        self.set_error_count(errors)
 
     # -----------------------------------------------------------------------
     # Pobiera informacje na temat wykrytych w czasie transmisji błędów.
@@ -32,4 +39,20 @@ class Receiver:
     # Nic.
     # -----------------------------------------------------------------------
     def set_error_count(self, error_count: numpy.uint):
-        return
+        self.error_count += error_count
+
+    # -----------------------------------------------------------------------
+    # Zapisuje statystyki do pliku *.csv
+    #
+    # Parametry:
+    # file_name - nazwa pliku, do którego zapisane zostaną statystyki
+    #
+    # Zwraca:
+    # Nic.
+    # -----------------------------------------------------------------------
+    def save_statistics(self, file_name: str):
+        with open(file_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Received Message', 'Original Message'])
+            for received_message, original_message in self.received_messages:
+                writer.writerow([received_message, original_message])
