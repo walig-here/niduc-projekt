a0c51074afbe33882f8c076c53215ea5ad5e7b28
walig-here <daw.walig@gmail.com>
2023-04-22 19:00:46 +0200

    Etap II
    Przygotowanie drugiego etapu implementacji.

--- arq/Configuration.py
+++ arq/Configuration.py
@@ -3,7 +3,7 @@ from arq import Channel as ChannelModule
 from arq import Decoder as DecoderModule
 
 
-# Wczytuje konfigurację aplikacji
+# Wczytuje konfigurację aplikacji - z simulation.config
 # - ilość prób
 # - nazwa pliku wyjściowego
 # - długość wiadomości
@@ -11,14 +11,14 @@ def configure_simulation():
     pass
 
 
-# Wczytuje konfigurację kanału
+# Wczytuje konfigurację kanału - channel.config
 # - BER
 # - parametry kanału
 def configure_channel(channel: ChannelModule.Channel):
     pass
 
 
-# Wczytuje konfiguację kodera/dekodera
+# Wczytuje konfiguację kodera/dekodera - encoding.config
 # - ilość segmentów
 # - typ kodowania
 def configure_encoding(encoder: EncoderModule.Encoder, decoder: DecoderModule.Decoder):
